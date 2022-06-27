005ba7b8dc3f492ade61da2dcea24962cb8fc925
Joshua <joshsmayhew@gmail.com>
2022-06-27 16:38:13 +0700

    commit tree 6cef831afd846cef6ff5521b2fc366377c888301

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-function addingEventListener() {
+
+const input = document.getElementById('input');
+
+
+function clickAlert () {
+
+        alert('I was clicked!');
+
 }
+
+function addingEventListener(e, cb) {
+
+    return input.addEventListener(e, cb);
+
+
+
+};
+
+addingEventListener('click', clickAlert);
+
+
